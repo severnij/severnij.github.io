bcab11694be3a2d111596b41e540d083ad60c383
Vadim Vitkauskas <severnij@rambler.ru>
2022-12-09 11:36:35 +0300

    Add ServiceWorker Install

--- worker.js
+++ worker.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = `PWA1-cache-v1`;
+const cacheName = `PWA1-cache-v1`;
 
-// Use the install event to pre-cache all initial resources.
-self.addEventListener('install', event => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    cache.addAll(['/']);
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(contentToCache);
   })());
 });
